9deb131403398809d1433a052b6953c54786f919
Fredisar <96703216+Fredisar@users.noreply.github.com>
2025-08-17 22:45:48 +0000

    Update Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,10 +1,5 @@
 package app;
 import fr.egocentrique.*;
-import fr.egocentrique.Morceau;
-import fr.egocentrique.Playlist;
-import fr.egocentrique.Utilisateur;
-
-import static java.lang.System.*;
 
 public class Main {
     public static void main(String[] args) {
